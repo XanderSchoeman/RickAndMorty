a038abdb388f77bb30700be6b0767a9201a092df
Xander Schoeman <Xschoeman@jhb.dvt.co.za>
2024-01-14 16:17:09 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Xander Schoeman on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
